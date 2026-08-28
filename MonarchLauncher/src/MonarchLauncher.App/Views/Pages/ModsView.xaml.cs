c6f7e93bea27d1da3de3aa75ff9dd94da676e922
fyrxc <fyrxcot@gmail.com>
2026-08-28 02:09:27 -0400

    feat: scan installed mods when page opens

--- MonarchLauncher/src/MonarchLauncher.App/Views/Pages/ModsView.xaml.cs
+++ MonarchLauncher/src/MonarchLauncher.App/Views/Pages/ModsView.xaml.cs
@@ -1,11 +1,24 @@
+using System.Windows;
 using System.Windows.Controls;
+using MonarchLauncher.App.ViewModels;
 
 namespace MonarchLauncher.App.Views.Pages;
 
 public partial class ModsView : UserControl
 {
+    private bool _loadedOnce;
+
     public ModsView()
     {
         InitializeComponent();
+    }
+
+    private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        if (_loadedOnce || DataContext is not ModsViewModel viewModel)
+            return;
+
+        _loadedOnce = true;
+        await viewModel.RefreshAsync();
     }
 }

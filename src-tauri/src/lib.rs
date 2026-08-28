b27bc8d8f1ad2138f2be9419afd036c94671c1bf
fyrxc <fyrxcot@gmail.com>
2026-08-28 15:14:12 -0400

    feat: register automatic server directory command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,19 @@
+pub mod commands;
 pub mod models;
 pub mod servers;
 
+use commands::LauncherState;
+use servers::DzsaServerDirectory;
+use std::sync::Arc;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let server_directory =
+        DzsaServerDirectory::new().expect("failed to initialize server directory");
+
     tauri::Builder::default()
+        .manage(LauncherState::new(Arc::new(server_directory)))
+        .invoke_handler(tauri::generate_handler![commands::get_servers])
         .run(tauri::generate_context!())
         .expect("error while running Monarch Lanucher");
 }
